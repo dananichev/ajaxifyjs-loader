b3cc17a7c0f7f63b746b8b6473c162b719656772
Fr3nzy <a@qozz.ru>
2013-06-14 18:08:37 +0600

    basic folder structure changed due using bower

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -1,14 +1,14 @@
 requirejs.config({
     //By default load any module IDs from js/lib
-    baseUrl: '../lib',
+    baseUrl: '../src',
     //except, if the module ID starts with "app",
     //load it from the js/app directory. paths
     //config is relative to the baseUrl, and
     //never includes a ".js" extension since
     //the paths config could be for a directory.
     paths: {
-        jquery: 'jquery-2.0.1.min',
-        mockjax: 'jquery.mockjax'
+        jquery: 'vendor/jquery-2.0.1.min',
+        mockjax: 'vendor/jquery.mockjax'
     },
     shim: {
         'mockjax': {
